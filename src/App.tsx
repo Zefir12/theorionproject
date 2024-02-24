24d8628efae9c60d9e850a29607e2f7e49234689
Patryk Borowiec <56939183+Zefir12@users.noreply.github.com>
2024-02-24 05:25:57 +0100

    fix i guess

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
-  useEffect(() => {
-    console.log(import.meta.env.VITE_SUPABASE_TOKEN)
-}, []);
 
   return (
     <>
