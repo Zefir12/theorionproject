2885d7e8cb4b66f8b2c109c5789794c9235755d6
Patryk Borowiec <56939183+Zefir12@users.noreply.github.com>
2024-02-29 07:56:06 +0100

    testing secrets

--- src/pages/testPage.tsx
+++ src/pages/testPage.tsx
@@ -1,11 +1,43 @@
+import { useState } from 'react';
+import { createClient } from '@supabase/supabase-js';
+import { supabaseUrl, supabaseKey } from '../tokens.json'
 
+
+
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 
 
 export default function TestPage() {
+    const [user, setUser] = useState<any>(null);
+
+
+    const signIn = async () => {
+        const { error } = await supabase.auth.signInWithPassword({
+          email: 'email',
+          password: '[pswd]'
+        });
+        console.log(supabaseKey, supabaseUrl)
+    
+        if (error) {
+          console.error('Error signing in:', error.message);
+        } else {
+          supabase.auth.getUser().then(res => {setUser(res); console.log(res)})
+        }
+      };
+
+    const signOut = async () => {
+        await supabase.auth.signOut();
+        setUser(null);
+      };
+
     return (
         <div className="flex flex-col items-center">
-            sadek
+            {user ? (
+                <button onClick={signOut}>Sign out</button>
+            ) : (
+                <button onClick={signIn}>Sign in</button>
+            )}
         </div>
     );
 }
