3f14a9a3d46685857c3a9a8def82bd55a685ef42
Patryk Borowiec <56939183+Zefir12@users.noreply.github.com>
2024-03-09 23:54:18 +0100

    test 55

--- src/pages/mainPage.tsx
+++ src/pages/mainPage.tsx
@@ -7,7 +7,7 @@ export default function MainPage() {
     const [count, setCount] = useState(0)
 
     return (
-      <div className="flex flex-col bg-red-800 h-64 w-64">
+      <div>
         <div>
           <a href="https://vitejs.dev" target="_blank">
             <img src={viteLogo} className="logo" alt="Vite logo" />
